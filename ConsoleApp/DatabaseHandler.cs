9d4eee657731d7700dbf170d0a25af84f351660c
Omer Yassir <omerym10@gmail.com>
2024-05-05 23:57:07 +0300

    Insert Infant

--- ConsoleApp/DatabaseHandler.cs
+++ ConsoleApp/DatabaseHandler.cs
@@ -9,67 +9,71 @@ namespace ConsoleApp
 {
     public static class DatabaseHandler
     {
+        public static void InsertInfant(string connectionString,string name,int companionId,DateTime birthDate,string fnum)
+        {
+            using SqlConnection connection = new(connectionString);
+            string queryString = "Insert Into Infant values(@name,@companionId,@birthDate,@fnum);";
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@companionId", companionId);
+            command.Parameters.AddWithValue("@birthDate", birthDate);
+            command.Parameters.AddWithValue("@fnum", fnum);
+            connection.Open();
+            command.ExecuteNonQuery();
+        }
         public static void InsertAdmin(string connectionString, string name)
         {
-            using (SqlConnection connection = new(connectionString))
-            {
-                string queryString = "Insert Into Admin values(@name);";
-                SqlCommand command = new SqlCommand(queryString, connection);
-                command.Parameters.AddWithValue("@name", name);
-                connection.Open();
-                command.ExecuteNonQuery();
-            }
+            using SqlConnection connection = new(connectionString);
+            string queryString = "Insert Into Admin values(@name);";
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@name", name);
+            connection.Open();
+            command.ExecuteNonQuery();
 
         }
         public static void InsertCustomer(string connectionString, string name, string passport, string nationality)
         {
-            using (SqlConnection connection = new(connectionString))
-            {
-                string queryString = "Insert Into Customer values(@name,@passport,@nationality);";
-                SqlCommand command = new SqlCommand(queryString, connection);
-                command.Parameters.AddWithValue("@name", name);
-                command.Parameters.AddWithValue("@passport", passport);
-                command.Parameters.AddWithValue("@nationality", nationality);
-                connection.Open();
-                command.ExecuteNonQuery();
-            }
+            using SqlConnection connection = new(connectionString);
+            string queryString = "Insert Into Customer values(@name,@passport,@nationality);";
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@passport", passport);
+            command.Parameters.AddWithValue("@nationality", nationality);
+            connection.Open();
+            command.ExecuteNonQuery();
 
         }
         public static IEnumerable<string[]> ReadTable(string connectionString, string tableName)
         {
-            using (SqlConnection connection = new(connectionString))
+            using SqlConnection connection = new(connectionString);
+            string queryString = $"SELECT * FROM {tableName};";
+            SqlCommand command = new SqlCommand(queryString, connection);
+            connection.Open();
+            using (SqlDataReader reader = command.ExecuteReader())
             {
-                string queryString = $"SELECT * FROM {tableName};";
-                SqlCommand command = new SqlCommand(queryString, connection);
-                connection.Open();
-                using (SqlDataReader reader = command.ExecuteReader())
+                while (reader.Read())
                 {
-                    while (reader.Read())
+                    string[] values = new string[reader.FieldCount];
+                    for (int i = 0; i < values.Length; i++)
                     {
-                        string[] values = new string[reader.FieldCount];
-                        for (int i = 0; i < values.Length; i++)
-                        {
-                            values[i] = reader[i].ToString()??"";
-                        }
-                        yield return values;
+                        values[i] = reader[i].ToString() ?? "";
                     }
+                    yield return values;
                 }
             }
 
         }
         public static IEnumerable<Tuple<int,string,string,string>> ReadCustomers(string connectionString)
         {
-            using(SqlConnection connection = new(connectionString))
+            using SqlConnection connection = new(connectionString);
+            string queryString = "SELECT * FROM Customer;";
+            SqlCommand command = new SqlCommand(queryString, connection);
+            connection.Open();
+            using (SqlDataReader reader = command.ExecuteReader())
             {
-                string queryString = "SELECT * FROM Customer;";
-                SqlCommand command = new SqlCommand(queryString, connection);
-                connection.Open();
-                using (SqlDataReader reader = command.ExecuteReader())
+                while (reader.Read())
                 {
-                    while (reader.Read())
-                    {
-                        yield return new(reader.GetInt32(0),reader.GetString(1),reader.GetString(2),reader.GetString(3));
-                    }
+                    yield return new(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
                 }
             }
         }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             string connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FlightSystem;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
-            var admins = DatabaseHandler.ReadTable(connection, "Admin");
+            DatabaseHandler.InsertInfant(connection, "Fayez", 1, DateTime.Parse("2022/11/19"), "Ab809");
+            var admins = DatabaseHandler.ReadTable(connection, "Infant");
             foreach (var admin in admins)
             {
                 for (int i = 0;i<admin.Length;i++)
