cd7a25a6af2fadd72b8ea40ea236feb472cf50d8
Omer Yassir <omerym10@gmail.com>
2024-05-06 00:00:49 +0300

    dfshf

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,6 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             string connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FlightSystem;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
-            DatabaseHandler.InsertInfant(connection, "Fayez", 1, DateTime.Parse("2022/11/19"), "Ab809");
             var admins = DatabaseHandler.ReadTable(connection, "Infant");
             foreach (var admin in admins)
             {
